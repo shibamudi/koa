a6c02c4c22b551182d1916ea046008e1735d481e
shibamudi <919177684@qq.com>
2017-06-13 19:03:39 +0800

    不再询问 koa 版本

--- meta.js
+++ meta.js
@@ -14,13 +14,6 @@ module.exports = {
     author: {
       'type': 'string',
       'message': 'Author'
-    },
-    koaVersion: {
-      'type': 'list',
-      'required': true,
-      'message': 'Koa version',
-      'choices': ['1.x', '2.x'],
-      'default': 0
     }
   },
   completeMessage: '{{#inPlace}}To get started:\n\n  npm install # Or yarn\n  npm run dev{{else}}To get started:\n\n  cd {{destDirName}}\n  npm install # Or yarn\n  npm run dev{{/inPlace}}'
